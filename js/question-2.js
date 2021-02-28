b5d7cacacff99bf3314e871422d65016b00c9263
Svanhild Sleire <svanesdesign@gmail.com>
2021-02-28 11:50:31 +0100

    Create question-2.js

---
+++ js/question-2.js
@@ -0,0 +1,50 @@
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const proxy = "https://noroffcors.herokuapp.com/";
+
+const corsUrl = proxy + url;
+
+const resultsContainer = document.querySelector(".results");
+
+async function getGames() {
+  try {
+    const response = await fetch(corsUrl);
+    const gamesResults = await response.json();
+    const games = gamesResults.results;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+      if (i === 8) {
+        break;
+      }
+
+      const name = games[i].name;
+
+      let ratingOfGame = "No ratings yet";
+
+      if (games[i].rating) {
+        ratingOfGame = "Rating: " + games[i].rating;
+      }
+
+      let numberOfTags = "";
+
+      if (games[i].tags.length) {
+        numberOfTags = "Number of tags: " + games[i].tags.length;
+      }
+
+      resultsContainer.innerHTML += `<div class="result">
+     <h2>${name}</h2>
+       <ul>
+        <li>${ratingOfGame}</li>
+        <li>${numberOfTags}</li>
+       </ul>
+    </div>`;
+    }
+  } catch (error) {
+    resultsContainer.innerHTML = displayError(
+      "An error occurred when calling the API"
+    );
+  }
+}
+getGames();
